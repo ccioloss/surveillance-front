3167e0bfa0e2fb91a9802e6eeb6c92e6c604b5a3
Render <renderXR@gmail.com>
2021-10-19 15:25:47 +0200

    Create color.js
    added color.js for color theme.

---
+++ colors.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#d7ffff',
+      main: '#a4dce5',
+      dark: '#73aab3',
+      contrastText: '#232426',
+    },
+    secondary: {
+      light: '#ff8382',
+      main: '#f74f55',
+      dark: '#be092c',
+      contrastText: '#ffe5ea',
+    },
+  },
+});
